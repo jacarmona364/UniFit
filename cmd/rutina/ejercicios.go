576715e58a55375208ce9ccf82d43de0da8a8bb1
Jose Maria Gomez Laynez <152492107+gosema@users.noreply.github.com>
2025-11-23 12:32:52 +0100

    feat: #23 Se añaden los tipos de dificultad para tratar la intensidad
    He añadido los tres tipos de dificultades que podemos obtener de la web para estimar la intensidad. 
    Lo defino como un type constante ya que es necesaria su inmutabilidad para que en ninguna parte del código sea modificado alguno de los tipos o añadido uno nuevo.
    También los defino como strings para facilitar al programador cuando necesite guardar o comprobar los tipos de dificultad de la web como cadenas de caracteres.

--- cmd/rutina/ejercicios.go
+++ cmd/rutina/ejercicios.go
@@ -1,3 +1,13 @@
 package rutina
 
+// Ejercicios que componen un grupo muscular
+
+// La dificultad puede ser de tres tipos
+type Dificultad string
+const (
+	Beginner     Dificultad = "BEGINNER"
+	Intermediate Dificultad = "INTERMEDIATE"
+	Advanced     Dificultad = "ADVANCED"
+)
+
 
