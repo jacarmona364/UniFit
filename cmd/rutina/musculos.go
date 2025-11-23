50857c301ef872bd722349cc4eb0e54343242046
Jose Maria Gomez Laynez <152492107+gosema@users.noreply.github.com>
2025-11-23 13:13:01 +0100

    feat: #21 Se añade la estructura grupo muscular para los datos de la web
    Como hemos identificado los datos de la web, tenemos varios grupos musculares. 
    Cada uno con un nombre por el que se hace referencia, por ello el uso de strings para el atributo nombre para guardar esta cadena de caracteres de la web.
    Y con ejercicios asociados, mediante la estructura de datos de un vector se pueden almacenar los ejercicios asociados al grupo muscular. De esta forma simplificamos al desarrollador el proceso de iteración sobre cada uno de los ejercicios.

--- cmd/rutina/musculos.go
+++ cmd/rutina/musculos.go
@@ -1,3 +1,9 @@
 package rutina
 
+//Grupos musculares de la web para ejercitar
+type GrupoMuscular struct {
+	Nombre     string     //nombre del grupo muscular de la web 
+	Ejercicios []Ejercicio // lista de ejericicos que lo compone
+}
+
 
