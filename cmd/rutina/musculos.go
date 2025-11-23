41265c62ce9e819ed0f87fceef6ea16f6b8fe403
Jose Maria Gomez Laynez <152492107+gosema@users.noreply.github.com>
2025-11-23 18:09:23 +0100

    fix: closes #21 Fin de commits de objetivo 2
    El commit 50857c3 debía cerrar el issue #21 pues terminaba de estructurar los grupos musculares y ejercicios, que era lo obtenido en la web.

--- cmd/rutina/musculos.go
+++ cmd/rutina/musculos.go
@@ -5,9 +5,11 @@ const LimiteSobrecarga = 100
 
 //Grupos musculares de la web para ejercitar
 type GrupoMuscular struct {
+	
 	Nombre     string     //nombre del grupo muscular de la web 
 	Ejercicios []Ejercicio // lista de ejericicos que lo compone
 	Sobrecarga bool // Estado que indica true si el musculo esta sobrecargado o false por defecto
+	
 }
 
 
