ae5a06257e575d9a39e257d035d3f7503c74d81f
Jose Maria Gomez Laynez <152492107+gosema@users.noreply.github.com>
2025-11-24 19:38:35 +0100

    feat: #24 Se añade el enum para los dias de la semana
    Para crear una buena planificacion debemos asignar una tarea a cada día de la semana. Para realizar esta tarea nos sirve de mucha ayuda tener un enum con los 7 tipos de dias de la semana para asociarlos a la planificacion e identificarlos con su nombre

--- cmd/rutina/planificacion.go
+++ cmd/rutina/planificacion.go
@@ -1,1 +1,14 @@
 package rutina 
+
+//Añado un enum para los distintos dias de la semana de la planificacion
+type DiaSemana string
+
+const (
+	Lunes     DiaSemana = "LUNES"
+	Martes    DiaSemana = "MARTES"
+	Miercoles DiaSemana = "MIERCOLES"
+	Jueves    DiaSemana = "JUEVES"
+	Viernes   DiaSemana = "VIERNES"
+	Sabado    DiaSemana = "SABADO"
+	Domingo   DiaSemana = "DOMINGO"
+)
