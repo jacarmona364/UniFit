d91cee1b0288f39cdd1651c0b4a0198cfb1af340
Jose Maria Gomez Laynez <152492107+gosema@users.noreply.github.com>
2025-11-25 11:46:32 +0100

    fix: #22 se renombra los atributos con nombres mas claros
    Tenemos un booleano que determina si el grupo muscular esta saturado o no y una carga acumulada que almacena en que estado se encuentra el grupo y si le queda poco para alcanzar la sobrecarga

--- cmd/rutina/musculos.go
+++ cmd/rutina/musculos.go
@@ -5,9 +5,10 @@ type GrupoMuscular struct {
 	
 	Nombre     string     //nombre del grupo muscular de la web 
 	Ejercicios []Ejercicio // lista de ejericicos que lo compone
+	
 	LimiteSobrecarga int // límite que no puede ser sobrepasado pues se produciría la sobrecarga
-	SobrecargaAcumulada int // valor que almacena como evoluciona la sobrecarga del grupo
-	Sobrecarga bool // Estado que indica true si el musculo esta sobrecargado o false por defecto
+	CargaAcumulada int // valor que almacena como evoluciona la carga del grupo
+	EstaSobrecargado bool // Estado que indica true si el musculo esta sobrecargado o false por defecto
 	
 }
 
