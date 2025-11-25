f985d7149b11b0b9578280da8cd805ff3dd5fd20
Jose Maria Gomez Laynez <152492107+gosema@users.noreply.github.com>
2025-11-25 18:32:06 +0100

    fix: #21 correccion de explicacion de comentario
    Se cambia la explicacion en los comentarios para expresar al desarrollador de forma mas clara que un grupo muscular tiene ejercicios disponibles que ejercitar obtenidos de la web y que la sobrecarga es un limite que cuando supera una persona alcanza el estado de sobrecargado y necesita descansar el grupo

--- cmd/rutina/grupo_muscular.go
+++ cmd/rutina/grupo_muscular.go
@@ -4,11 +4,11 @@ package rutina
 type GrupoMuscular struct {
 	
 	Nombre     string     //nombre del grupo muscular de la web 
-	Ejercicios []Ejercicio // lista de ejercicos que compone un grupo muscular
+	Ejercicios []Ejercicio // lista de ejercicos asociados al grupo muscular
 	
 	LimiteSobrecarga int // límite que no puede ser sobrepasado pues se produciría la sobrecarga
 	CargaAcumulada int // valor que almacena como evoluciona la carga del grupo
-	EstaSobrecargado bool // Estado que indica true si el musculo esta sobrecargado o false por defecto
+	EstaSobrecargado bool // Estado que indica true si el musculo esta sobrecargado
 	
 }
 
