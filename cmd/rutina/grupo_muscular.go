8c6dcd1c2fecfbfe2cac797876c0a4b0067899eb
Jose Maria Gomez Laynez <152492107+gosema@users.noreply.github.com>
2025-11-26 00:32:40 +0100

    feat: closes #24 Se añade el identificador único para grupo muscular
    Al analizar los datos de los que disponemos en la web en #21, hemos encontrado que ningun grupo muscular coincide en nombre. Por lo tanto no puede producirse una copia de musculos con el mismo nombre, cada uno es único y es obtenido de la web como una cadena de caracteres única que servirá como única fuente de verdad para los ejercicios

--- cmd/rutina/grupo_muscular.go
+++ cmd/rutina/grupo_muscular.go
@@ -3,6 +3,7 @@ package rutina
 //Grupos musculares de la web para ejercitar
 type GrupoMuscular struct {
 
+	Nombre string // Nombre único que posee cada grupo muscular
 	Ejercicios []Ejercicio // lista de ejercicos asociados al grupo muscular
 	
 }
