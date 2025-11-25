7c52fb37781eb4ebecd682dfa091f02baa54d21c
Jose Maria Gomez Laynez <152492107+gosema@users.noreply.github.com>
2025-11-26 00:27:07 +0100

    feat: #22 se añade un vector con ejercicios para cada grupo muscular
    Como hemos analizado en DDD necesitamos tener una coleccion de ejercicios en cada grupo muscular. Por ello la forma ideal de organizar esta estructura de datos es mediante un vector que organice todos los ejercicios.

--- cmd/rutina/grupo_muscular.go
+++ cmd/rutina/grupo_muscular.go
@@ -2,13 +2,8 @@ package rutina
 
 //Grupos musculares de la web para ejercitar
 type GrupoMuscular struct {
-	
-	Nombre     string     //nombre del grupo muscular de la web 
+
 	Ejercicios []Ejercicio // lista de ejercicos asociados al grupo muscular
-	
-	LimiteSobrecarga int // límite que no puede ser sobrepasado pues se produciría la sobrecarga
-	CargaAcumulada int // valor que almacena como evoluciona la carga del grupo
-	EstaSobrecargado bool // Estado que indica true si el musculo esta sobrecargado
 	
 }
 
