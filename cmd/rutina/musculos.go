b5b45894a141ca271532e2b48b76d28990cb0ccd
Jose Maria Gomez Laynez <152492107+gosema@users.noreply.github.com>
2025-11-25 11:38:45 +0100

    fix: #22 Se elimina la deuda de TODO
    En vez de una constante común para todos los grupos musculares(que es poco coherente ya que los musculos tienen distinto limite de sobrecarga) se establece un limite numerico para comprobar si las intensidades pueden superar este límite. 
    Se utiliza un entero para facilitar al desarrollador el cálculo y las estimaciones

--- cmd/rutina/musculos.go
+++ cmd/rutina/musculos.go
@@ -1,13 +1,11 @@
 package rutina
 
-// TODO: Definir el límite real. Pongo 100 temporalmente para que compile pero debe ser un valor del cliente
-const LimiteSobrecarga = 100
-
 //Grupos musculares de la web para ejercitar
 type GrupoMuscular struct {
 	
 	Nombre     string     //nombre del grupo muscular de la web 
 	Ejercicios []Ejercicio // lista de ejericicos que lo compone
+	LimiteSobrecarga int // límite que no puede ser sobrepasado pues se produciría la sobrecarga
 	SobrecargaAcumulada int // valor que almacena como evoluciona la sobrecarga del grupo
 	Sobrecarga bool // Estado que indica true si el musculo esta sobrecargado o false por defecto
 	
