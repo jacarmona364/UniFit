35d1c8a58c5190257e2ef1dd9bddbb35c79a75c2
Jose Maria Gomez Laynez <152492107+gosema@users.noreply.github.com>
2025-11-26 23:28:38 +0100

    feat: #24 Creamos el enum de los dias de la semana
    Necesitamos para identificar que grupo muscular se ejercita cada día. Para ello necesitamos identificar los dias de la semana con los que trabajaremos, un enum es la mejor forma ya que nos permite asociar cada dia a un numero y añadirlo posteriormente como clave del map de rutina semanal

--- internal/models/rutina.go
+++ internal/models/rutina.go
@@ -1,1 +1,15 @@
 package models
+
+// Definimos un tipo personalizado para los días (nuestro Enum)
+type DiaSemana int
+
+// Definimos las constantes usando iota para autoincrementar (Lunes=0, Martes=1, etc.)
+const (
+	Lunes DiaSemana = iota
+	Martes
+	Miercoles
+	Jueves
+	Viernes
+	Sabado
+	Domingo
+)
