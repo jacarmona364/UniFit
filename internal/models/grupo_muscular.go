3d242b3d69b8c5e05a93e60e27f2b29bb4f27a57
Jose Maria Gomez Laynez <152492107+gosema@users.noreply.github.com>
2025-11-26 23:14:46 +0100

    fix: #22 cambio de privacidad en los atributos
    Los atributos de ejercicios deben ser privados para encapsularlos y no modificarlos desde fuera de la clase ya que son datos obtenidos de la web. 
    Necesitamos un string que almacena la cadena de caracteres con la que es representado un grupo en la web, como hemos analizado en #21. Y añadimos un vector para agrupar todos los datos asociados a ejercicios como hemos deducido en #22.

--- internal/models/grupo_muscular.go
+++ internal/models/grupo_muscular.go
@@ -2,6 +2,6 @@ package models
 
 // Grupos musculares que encontramos en la web
 type GrupoMuscular struct {
-	Nombre     string      // nombre del grupo muscular por el que se identifica en la web
-	Ejercicios []Ejercicio // lista de ejercicios que contiene ese grupo
+	nombre     string      // nombre del grupo muscular por el que se identifica en la web
+	ejercicios []Ejercicio // lista de ejercicios que contiene ese grupo
 }
