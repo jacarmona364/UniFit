b634470e013818bdf1c01de2c4104349cf74091a
Jose Maria Gomez Laynez <152492107+gosema@users.noreply.github.com>
2025-11-26 22:50:43 +0100

    feat: #22 se añade el atributo nombre de grupo y sus ejercicios asociados
    Como hemos analizado en el dominio del problema tenemos muchos grupos musculares de los que solo sabemos sus nombres que son únicos en la web (no hay dos abdominales), usamos un string para almacenar esa cadena de caracteres. Además cada grupo tiene muchos ejercicios asociados por los que una buena idea es añadir estos ejercicios a un vector de tipo ejercicios.

--- internal/models/grupo_muscular.go
+++ internal/models/grupo_muscular.go
@@ -2,5 +2,6 @@ package models
 
 // Grupos musculares que encontramos en la web
 type GrupoMuscular struct {
-
+	Nombre     string      // nombre del grupo muscular por el que se identifica en la web
+	Ejercicios []Ejercicio // lista de ejercicios que contiene ese grupo
 }
