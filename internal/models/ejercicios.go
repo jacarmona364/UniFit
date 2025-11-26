302c6605caf04f4cc08c00dff03a6b78ab64ff12
Jose Maria Gomez Laynez <152492107+gosema@users.noreply.github.com>
2025-11-26 23:03:09 +0100

    feat: #23 Se añaden los tipos de dificultad de un ejercicio
    Como hemos analizado en #21, la única información que necesitamos de la web es la referida a la dificultad, que esta solo puede ser de tres tipos. Para ello la mejor solución es hacer un enum de strings que nos permite comparar estas cadenas con las dificultades de los ejercicios de la web

---
+++ internal/models/ejercicios.go
@@ -0,0 +1,9 @@
+package models
+
+// La dificultad de un ejercicio puede ser de tres tipos 
+type Dificultad string
+const (
+	Beginner     Dificultad = "BEGINNER"
+	Intermediate Dificultad = "INTERMEDIATE"
+	Advanced     Dificultad = "ADVANCED"
+)
