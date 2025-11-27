8ef5987baf270f7508f0516b530df0626628e12b
gosema <gomezlaynez@gmail.com>
2025-11-27 13:45:21 +0100

    feat: jacarmona364#24 Añadimos una estructura He creado un map que sirve para estructurar la información de los ejercicios asocidados a cada grupo muscular, como solo necesitamos los nombres de los grupos musculares jacarmona364#21 podemos utilizarlos como clave para asegurarnos que solo hay un grupo muscularcon nombre único.

--- internal/models/rutina.go
+++ internal/models/rutina.go
@@ -2,5 +2,5 @@ package models
 // Entidad 
 //Única por usuario
 type Rutina struct {
-	
+	catalogoEjercicios map[GrupoMuscular][]Ejercicio //conjunto de datos que tenemos de la web, ejercicios asociados a un grupo muscular
 }
