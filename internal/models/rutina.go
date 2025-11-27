b81191e3cff921595e35f2fe78ec24493c869ae1
gosema <gomezlaynez@gmail.com>
2025-11-27 13:50:48 +0100

    feat: jacarmona364#21 añadir grupos musculares Creo un enum que que controla los grupos musculares obtenidos de la web, de esta forma a cada grupo se le asocia un número con el que será identificado.

--- internal/models/rutina.go
+++ internal/models/rutina.go
@@ -1,6 +1,14 @@
 package models
-// Entidad 
-//Única por usuario
+type GrupoMuscular int
+const (
+	// iota empieza en 0.
+	Abductors GrupoMuscular = iota
+	Abs
+	Biceps
+	//... se deben añadir los grupos a trabajar
+)
+
+//Entidad única que gestiona los datos obtenidos
 type Rutina struct {
 	catalogoEjercicios map[GrupoMuscular][]Ejercicio //conjunto de datos que tenemos de la web, ejercicios asociados a un grupo muscular
 }
