d06a6e311837c2778e5a78d9b64201a78ea7c1dc
Jose Maria Gomez Laynez <152492107+gosema@users.noreply.github.com>
2025-11-27 14:02:42 +0100

    fix: #21 se elimina archivo de musculos
    Hemos cambiado la estructura del proyecto, por lo que el archivo ahora no tiene valor.

--- internal/models/grupo_muscular.go
+++
@@ -1,7 +0,0 @@
-package models
-
-// Grupos musculares que encontramos en la web
-type GrupoMuscular struct {
-	nombre     string      // nombre del grupo muscular por el que se identifica en la web
-	ejercicios []Ejercicio // lista de ejercicios que contiene ese grupo
-}
