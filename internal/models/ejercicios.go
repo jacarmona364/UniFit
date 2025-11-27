467cc480b988cca06a29f26ff6352c3822a2f531
gosema <gomezlaynez@gmail.com>
2025-11-27 13:53:43 +0100

    feat: jacarmona364#23 se añade la dificultad
    Un ejercicio tendrá una dificultad asociada que puede ser de unos de los tres tipos, este dato es de gran relevancia
    ya que de esta manera podemos controlar las intensidades en un futuro, identificando su cadena de caracteres si pertenece
    a uno de estos tres grupos.

--- internal/models/ejercicios.go
+++ internal/models/ejercicios.go
@@ -1,3 +1,11 @@
+package models
 
+// La dificultad de un ejercicio puede ser de tres tipos 
+type Dificultad string
+const (
+	Beginner     Dificultad = "BEGINNER"
+	Intermediate Dificultad = "INTERMEDIATE"
+	Advanced     Dificultad = "ADVANCED"
+)
 
 
