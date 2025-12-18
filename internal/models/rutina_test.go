ec991d1b666dd2b0dae809693458ff98fc7c5550
José Antonio Carmona Molina <83080951+jacarmona364@users.noreply.github.com>
2025-12-18 03:20:49 +0100

    feat: Agrego los test para la clase rutina, closes #25
    Tomamos esta decisión para poder testear el código relativo a la rutina, siguiendo los principios especificados previamente.

--- internal/models/rutina_test.go
+++ internal/models/rutina_test.go
@@ -1,1 +1,43 @@
 
+package models
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+//GOOD PATH
+
+func TestRutina_GenerarSemana_AsignacionCorrecta(t *testing.T) {
+  
+	rutina := NuevaRutina()
+	
+	ejPecho, _ := NuevoEjercicio("Press Banca", Intermediate)
+	ejPierna, _ := NuevoEjercicio("Sentadilla", Advanced)
+
+
+	semana, err := rutina.GenerarSemana()
+
+	assert.NoError(t, err)
+	assert.NotNil(t, semana)
+
+	assert.NotEmpty(t, semana["Lunes"], "El lunes debería tener ejercicios")
+	assert.Equal(t, 1, len(semana["Lunes"]))
+	
+		assert.NotEmpty(t, semana["Miércoles"], "El miércoles debería tener ejercicios")
+	
+	assert.Empty(t, semana["Martes"], "El martes no añadimos nada, debería estar vacío")
+}
+
+//SAD PATH
+
+func TestRutina_GenerarSemana_ErrorSiVacia(t *testing.T) {
+	rutina := NuevaRutina()
+
+	plan, err := rutina.GenerarSemana()
+
+	assert.Error(t, err)
+	assert.Nil(t, plan)
+  
+	assert.Is(t, err, ErrRutinaVacia, "Debe avisar si intentamos generar rutina sin ejercicios")
+}
