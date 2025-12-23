91669ec7ac14741474c965080a5a2b53d2191e53
José Antonio Carmona Molina <83080951+jacarmona364@users.noreply.github.com>
2025-12-24 00:02:45 +0100

    chore: Corrijo los test cambiando el nombre, closes #38
    Tener errores que sean poco claros y no especifiquen de manera correcta de donde viene el fallo los hace inútiles, de manera que lo mejoramos para cumplir con lo establecido en #38 .

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -3,27 +3,20 @@ package errors
 import "errors"
 
 var (
-	
-	// Errores Generales
-	ErrReadHTML = errors.New("error al leer el flujo de datos HTML")
+// Errores Generales
+	ErrLecturaHTML = errors.New("error al leer el flujo de datos HTML")
 
-	
-	// Errores de la Página Principal
-	// Basado en: <div class="mainpage-category-list">
-	ErrNoMainPageList = errors.New("no se encontró el contenedor principal '.mainpage-category-list'")
-	
-	// Basado en: <div class="category-name">
-	ErrNoCategoryNameClass = errors.New("no se encontró la clase '.category-name' para el nombre del grupo")
+	// Errores Index (Rutinas)
+	// <div class="mainpage-category-list">
+	ErrNoListaCategorias = errors.New("no se encontró el contenedor principal '.mainpage-category-list'")
+	// <div class="category-name">
+	ErrNoClaseNombreCategoria = errors.New("no se encontró la clase '.category-name' para el nombre del grupo")
 
-	
-	// Errores de la Página de Detalle
-	// Basado en: <div class="view-exercise-term-list ...">
-	ErrNoExerciseListContainer = errors.New("no se encontró el contenedor de lista '.view-exercise-term-list'")
-
-	// Basado en: <div class="node-title">
-	ErrNoNodeTitleClass = errors.New("no se encontró la clase '.node-title' para el título del ejercicio")
-
-	// Basado en: <label>Exp. Level</label>
-	ErrNoExpLevelLabel = errors.New("no se encontró la etiqueta '<label>Exp. Level</label>'")
-	
+	// Errores Detalle (Ejercicios)
+	// <div class="view-exercise-term-list ...">
+	ErrNoContenedorEjercicios = errors.New("no se encontró el contenedor de lista '.view-exercise-term-list'")
+	// <div class="node-title">
+	ErrNoClaseTituloNodo = errors.New("no se encontró la clase '.node-title' para el título del ejercicio")
+	// <label>Exp. Level</label>
+	ErrNoEtiquetaNivel = errors.New("no se encontró la etiqueta '<label>Exp. Level</label>'")
 )
