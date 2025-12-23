8b7405cfe48fe839b397a7b2e1ecbc6661176cb8
José Antonio Carmona Molina <83080951+jacarmona364@users.noreply.github.com>
2025-12-23 23:39:06 +0100

    feat: Añado errores acordes al testeo del scrapeo, closes #38
    Los errores previos eran relativos a la lógica de negocio orientada al algoritmo de la aplicación, ahora comienzan hablando de la lógica relativa a la extracción de la información.

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -1,9 +1,29 @@
-package models
+package errors
 
 import "errors"
 
 var (
-	ErrNombreVacio        = errors.New("el nombre del ejercicio no puede estar vacío")
-	ErrDificultadInvalida = errors.New("el nivel de dificultad proporcionado no es reconocido")
-	ErrGrupoSinEjercicios = errors.New("no se encontraron ejercicios para el grupo muscular solicitado")
+	
+	// Errores Generales
+	ErrReadHTML = errors.New("error al leer el flujo de datos HTML")
+
+	
+	// Errores de la Página Principal
+	// Basado en: <div class="mainpage-category-list">
+	ErrNoMainPageList = errors.New("no se encontró el contenedor principal '.mainpage-category-list'")
+	
+	// Basado en: <div class="category-name">
+	ErrNoCategoryNameClass = errors.New("no se encontró la clase '.category-name' para el nombre del grupo")
+
+	
+	// Errores de la Página de Detalle
+	// Basado en: <div class="view-exercise-term-list ...">
+	ErrNoExerciseListContainer = errors.New("no se encontró el contenedor de lista '.view-exercise-term-list'")
+
+	// Basado en: <div class="node-title">
+	ErrNoNodeTitleClass = errors.New("no se encontró la clase '.node-title' para el título del ejercicio")
+
+	// Basado en: <label>Exp. Level</label>
+	ErrNoExpLevelLabel = errors.New("no se encontró la etiqueta '<label>Exp. Level</label>'")
+	
 )
