d55bff75326e0e19de71dc2a6d70bcf2f1639ad6
José Antonio Carmona Molina <83080951+jacarmona364@users.noreply.github.com>
2025-12-28 01:25:40 +0100

    fix: Cambio la declaración de los errores a constante, closes #38
    Dado que los errores deben ser inmutables y en Go no se puede simplemente cambiar los errores a tipo constante en lugar de variable, creamos nuestro propio tipo basado en string e implementamos la interfaz error.

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -1,22 +1,25 @@
 package errors
 
-import "errors"
+type ErrorScraper string
 
-var (
-// Errores Generales
-	ErrLecturaHTML = errors.New("error al leer el flujo de datos HTML")
+func (e ErrorScraper) Error() string {
+	return string(e)
+}
 
+const (
+	// Errores Generales
+	ErrLecturaHTML = ErrorScraper("error al leer el flujo de datos HTML")
+	
 	// Errores Index (Rutinas)
 	// <div class="mainpage-category-list">
-	ErrNoListaCategorias = errors.New("no se encontró el contenedor principal '.mainpage-category-list'")
+	ErrNoListaCategorias = ErrorScraper("no se encontró el contenedor principal '.mainpage-category-list'")	
 	// <div class="category-name">
-	ErrNoClaseNombreCategoria = errors.New("no se encontró la clase '.category-name' para el nombre del grupo")
-
+	ErrNoClaseNombreCategoria = ErrorScraper("no se encontró la clase '.category-name' para el nombre del grupo")
+	
 	// Errores Detalle (Ejercicios)
 	// <div class="view-exercise-term-list ...">
-	ErrNoContenedorEjercicios = errors.New("no se encontró el contenedor de lista '.view-exercise-term-list'")
+	ErrNoContenedorEjercicios = ErrorScraper("no se encontró el contenedor de lista '.view-exercise-term-list'")
 	// <div class="node-title">
-	ErrNoClaseTituloNodo = errors.New("no se encontró la clase '.node-title' para el título del ejercicio")
+	ErrNoClaseTituloNodo = ErrorScraper("no se encontró la clase '.node-title' para el título del ejercicio")
 	// <label>Exp. Level</label>
-	ErrNoEtiquetaNivel = errors.New("no se encontró la etiqueta '<label>Exp. Level</label>'")
-)
+	ErrNoEtiquetaNivel = ErrorScraper("no se encontró la etiqueta '<label>Exp. Level</label>'"))
